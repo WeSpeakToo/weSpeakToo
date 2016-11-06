656a32d090a805e7b64f2c7e4be821ac9ef09b50
katerabinowitz <cathryn.rabinowitz@gmail.com>
2016-11-06 08:00:25 -0500

    test out json source

---
+++ speakerList.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    
+    $('#speakerList').DataTable( {
+        "ajax": 'speakers.json',
+        "order": [[0, "asc"]],
+        "columnDefs": [
+                {
+                    "render": function (data, type, row) {
+                        
+                        var link = convertToLink(data);
+                        
+                        return link;
+                        
+                    },
+                    "targets": 1
+                }
+            ]
+    } );
+    
+} );
+
+function convertToLink(link) {
+    
+    var html = '<a href=https://twitter.com/"' + link + '">' + link + "</a>";
+    
+    return html;
+    
+}
