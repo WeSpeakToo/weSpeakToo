c37b2d8b2738addacf4fe469a0380d7f3c746091
katerabinowitz <cathryn.rabinowitz@gmail.com>
2017-01-02 17:26:16 -0500

    test dc page

---
+++ DCspeakerList.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    
+    $('#speakerList').DataTable( {
+        "ajax": 'speakers.json',
+        "order": [[0, "asc"]],
+        "lengthMenu": [10, 25, 50, 75, 100],
+        "columnDefs": [
+                {
+                    "render": function (data, type, row) {
+                        
+                        var link = convertToLink(data);
+                        
+                        return link;
+                        
+                    },
+                    "targets": 1
+                }
+            ]
+    } );
+    
+} );
+
+function convertToLink(link) {
+    
+    var html = '<a target="_blank" href=https://twitter.com/' + link + '>' + link + "</a>";
+    
+    return html;
+    
+}
--- speakerList.js
+++ speakerList.js
@@ -27,3 +27,63 @@ function convertToLink(link) {
     return html;
     
 }
+
+$(document).ready(function() {
+    
+    $('#DCspeakerList').DataTable( {
+        "ajax": './dc/speakers.json',
+        "order": [[0, "asc"]],
+        "lengthMenu": [10, 25, 50, 75, 100],
+        "columnDefs": [
+                {
+                    "render": function (data, type, row) {
+                        
+                        var link = convertToLink(data);
+                        
+                        return link;
+                        
+                    },
+                    "targets": 1
+                }
+            ]
+    } );
+    
+} );
+
+function convertToLink(link) {
+    
+    var html = '<a target="_blank" href=https://twitter.com/' + link + '>' + link + "</a>";
+    
+    return html;
+    
+}
+
+$(document).ready(function() {
+    
+    $('#AustinSpeakerList').DataTable( {
+        "ajax": './austin/speakers.json',
+        "order": [[0, "asc"]],
+        "lengthMenu": [10, 25, 50, 75, 100],
+        "columnDefs": [
+                {
+                    "render": function (data, type, row) {
+                        
+                        var link = convertToLink(data);
+                        
+                        return link;
+                        
+                    },
+                    "targets": 1
+                }
+            ]
+    } );
+    
+} );
+
+function convertToLink(link) {
+    
+    var html = '<a target="_blank" href=https://twitter.com/' + link + '>' + link + "</a>";
+    
+    return html;
+    
+}
