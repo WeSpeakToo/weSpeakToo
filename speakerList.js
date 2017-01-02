c26fb8de06edb55dbb76729ccec0eee636e2b858
katerabinowitz <cathryn.rabinowitz@gmail.com>
2017-01-02 17:54:20 -0500

    austin js list

--- speakerList.js
+++ speakerList.js
@@ -1,35 +1,5 @@
 $(document).ready(function() {
     
-    $('#speakerList').DataTable( {
-        "ajax": 'speakers.json',
-        "order": [[0, "asc"]],
-        "lengthMenu": [10, 25, 50, 75, 100],
-        "columnDefs": [
-                {
-                    "render": function (data, type, row) {
-                        
-                        var link = convertToLink(data);
-                        
-                        return link;
-                        
-                    },
-                    "targets": 1
-                }
-            ]
-    } );
-    
-} );
-
-function convertToLink(link) {
-    
-    var html = '<a target="_blank" href=https://twitter.com/' + link + '>' + link + "</a>";
-    
-    return html;
-    
-}
-
-$(document).ready(function() {
-    
     $('#DCspeakerList').DataTable( {
         "ajax": './DC/speakers.json',
         "order": [[0, "asc"]],
