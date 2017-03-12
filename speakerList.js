a696e1457b34b984651cc970c2c064ec48baf7b1
katerabinowitz <cathryn.rabinowitz@gmail.com>
2017-03-12 15:00:32 -0400

    add early stages baltimore

--- speakerList.js
+++ speakerList.js
@@ -55,6 +55,36 @@ $(document).ready(function() {
     
 } );
 
+$(document).ready(function() {
+    
+    $('#bmoreSpeakerList').DataTable( {
+        "ajax": './baltimore/speakers.json',
+        "order": [[0, "asc"]],
+        "lengthMenu": [10, 25, 50, 75, 100],
+        "columnDefs": [
+                {
+                    "render": function (data, type, row) {
+                        
+                        var link = convertToLink(data);
+                        
+                        return link;
+                        
+                    },
+                    "targets": 1
+                },
+                {
+                "render": function ( data, type, row ) {
+                    return "<a target='_blank' href='" + row[4]+"'>"+row[0]+"</a>";
+                },
+                    "targets": 0
+                },
+                { 
+                    "visible": false,  "targets": 4 }
+            ]
+    } );
+    
+} );
+
 function convertToLink(link) {
     
     var html = '<a target="_blank" href=https://twitter.com/' + link + '>' + link + "</a>";
